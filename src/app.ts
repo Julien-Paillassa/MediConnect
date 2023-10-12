6f13d31f09e956f7ef78fe5535d38cf0f821c579
Matleftod <97225801+Matleftod@users.noreply.github.com>
2023-10-12 14:35:15 +0200

    build: add stripe + linter + swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,39 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc, { Options } from 'swagger-jsdoc';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Configuration Swagger JSDoc
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Votre API',
+    version: '1.0.0',
+    description: 'Documentation de votre API',
+  },
+};
+
+const options: Options = {
+  swaggerDefinition,
+  apis: ['./routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Endpoint racine
+ *     description: Renvoie un message de bienvenue
+ *     responses:
+ *       200:
+ *         description: Réponse réussie
+ */
 app.get('/', (req, res) => {
   res.send('Hello, Express with TypeScript!');
 });
---
+++ test.ts
@@ -0,0 +1,9 @@
+// test.ts
+
+const x = 42
+
+function add(a: number, b: number){
+  return a + b
+}
+
+console.log("Hello, World")
