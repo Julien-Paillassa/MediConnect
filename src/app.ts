f31847f9f292cf26faf4537286c3bee68f329ea2
Julien-Paillassa <julien.paillassa@gmail.com>
2023-10-13 10:21:21 +0200

    init database pg

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Express, type NextFunction, type Request, type Response } from 'express'
 import swaggerUi from 'swagger-ui-express'
 import swaggerJSDoc, { type Options } from 'swagger-jsdoc'
+import AppDataSource from './data-source'
 import 'reflect-metadata'
 
 const app: Express = express()
@@ -23,6 +24,12 @@ const options: Options = {
 const swaggerSpec = swaggerJSDoc(options)
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+AppDataSource.initialize()
+  .then(() => {
+    // here you can start to work with your database
+  })
+  .catch((error) => { console.log(error) })
 
 /**
  * @swagger
