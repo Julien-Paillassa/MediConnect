a364078d3dc9fbdee25b3ab888c165f253b04833
Julien-Paillassa <julien.paillassa@gmail.com>
2023-11-03 12:19:02 +0100

    fix: rebase code

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,23 +1,11 @@
 import { type NextFunction, type Request as ExpressRequest, type Response as ExpressResponse } from 'express'
-import * as UserServices from '../services/user.service'
+import * as UserService from '../services/user.service'
 import bcrypt from 'bcrypt'
-
-export function create (req: ExpressRequest, res: ExpressResponse, next: NextFunction): void {
-  const password = req.body.password
-  const hashedPassword = bcrypt.hashSync(password, 10)
-
-  UserServices.create({ name: req.body.name, email: req.body.email, password: hashedPassword, subscription: req.body.subscriptionId }).then((user) => {
-    res.status(201).send(user)
-  }
-  ).catch((err) => {
-    next(err)
-  })
-}
 
 export function update (req: ExpressRequest, res: ExpressResponse, next: NextFunction): void {
   const password = req.body.password
   const hashedPassword = bcrypt.hashSync(password, 10)
-  UserServices.update({ name: req.body.name, email: req.body.email, password: hashedPassword }).then((user) => {
+  UserService.update({ name: req.body.name, email: req.body.email, password: hashedPassword }).then((user) => {
     res.status(200).send(user)
   }).catch((err) => {
     next(err)
@@ -25,7 +13,7 @@ export function update (req: ExpressRequest, res: ExpressResponse, next: NextFun
 }
 
 export function remove (req: ExpressRequest, res: ExpressResponse, next: NextFunction): void {
-  UserServices.remove({ id: req.body.id }).then(() => {
+  UserService.remove({ id: req.body.id }).then(() => {
     res.status(204).send()
   }).catch((err) => {
     next(err)
